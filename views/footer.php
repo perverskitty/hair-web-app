ef4fbea87723192a2e28278bd43685d375681980
Peter Cheung <perverskitty@gmail.com>
2017-06-19 18:28:05 +0100

    Adding placeholder footer text to footer.php file.

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,10 @@
+    <footer>
+
+      <p>&copy; My Web App 2017</p>
+
+    </footer>
+
+
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
