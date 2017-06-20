127a7969f168b770f37316906782417afcc8a43f
Peter Cheung <perverskitty@gmail.com>
2017-06-20 13:30:46 +0100

    Adding email and password input validation to sign in action.

--- actions.php
+++ actions.php
@@ -2,10 +2,41 @@
 
   include("functions.php");
 
-  // action for when the 'sign in' button is pressed
+
+  // action for 'sign in'
   if ($_GET['action'] == "signin") {
     
-    print_r($_POST);
+    // validate email and password
+    $error = "";
+    
+    if (!$_POST['email']) {
+      
+      $error = "An email address is required";
+      
+    } else if (!$_POST['password']) {
+      
+      $error = "A password is required";
+      
+    } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+      
+      $error = "Please enter a valid email address";
+      
+    }
+    
+    if ($error != "") {
+      
+      echo $error;
+      exit();
+      
+    }
+    
+    // perform 'sign in'
+    if ($_POST['signinActive'] == "1") { 
+    
+    
+      
+    }
+    
     
   }
   
