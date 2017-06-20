f4eee50bcf6ea7241a00e98db01c67f809085e37
Peter Cheung <perverskitty@gmail.com>
2017-06-20 13:45:17 +0100

    Adding connection link to MySQL database in functions.php file.

--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php
 
+  // connection to my database
+  $link = mysqli_connect("localhost", "root", "", "hairdb");
+
+  // check for a connection error
+  if (mysqli_connect_errno()) {
+    
+    // print connection error and exit
+    print_r(mysqli_connect_error());
+    exit();
+  
+  }
+
 ?>
