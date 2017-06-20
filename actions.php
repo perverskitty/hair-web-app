726b7cb795ed9b1a163db58f44f0f9efac6cbcc0
Peter Cheung <perverskitty@gmail.com>
2017-06-20 15:21:42 +0100

    Adding placeholder action code for signing up a new user.

--- actions.php
+++ actions.php
@@ -3,7 +3,7 @@
   include("functions.php");
 
 
-  // action for 'sign in'
+  // Action for 'sign in'
   if ($_GET['action'] == "signin") {
     
     // validate email and password
@@ -33,12 +33,78 @@
     // perform 'sign in'
     if ($_POST['signinActive'] == "1") { 
     
+      $query = "SELECT * FROM users WHERE email = "
+      
+    }
+  }
+  
+
+  // Action for 'sign up'
+  if ($_GET['action'] == "signup") {
     
+    // validate email and password
+    $error = "";
+    
+    if (!$_POST['email']) {
+      
+      $error = "An email address is required";
+      
+    } else if (!$_POST['password']) {
+      
+      $error = "A password is required";
+      
+    } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+      
+      $error = "Please enter a valid email address";
       
     }
     
+    if ($error != "") {
+      
+      echo $error;
+      exit();
+      
+    }
+    
+    // query whether email address already exists in database
+    $query = "SELECT * FROM users WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
+    $result = mysqli_query($link, $query);
+    
+    if (mysqli_num_rows($result) > 0) {
+      
+      // notify if email already exists
+      $error = "That email address is already taken";
+      
+    } else {
+      
+      // sign up new user
+      $query = "INSERT INTO users (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
+      
+      if (mysqli_query($link, $query)) {
+        
+        // store user's password as a md5 hash
+        $query = "UPDATE users SET password = '".md5(md5(mysqli_insert_id($link)).$_POST['password'])."' WHERE id = ".mysqli_insert_id($link)." LIMIT 1";
+        mysqli_query($link, $query);
+        
+        // sign up success
+        echo 1;
+        
+      } else {
+        
+        // sign up fail
+        $error = "Couldn't create user - please try again later";
+        
+      }
+      
+    }
+    
+    if ($error != "") {
+      
+      echo $error;
+      exit();
+      
+    }
     
   }
-  
 
 ?>
