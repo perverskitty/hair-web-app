c7c62140bbfb87e8de86d41e7d2e96d259d1fcee
Peter Cheung <perverskitty@mac.com>
2017-06-26 11:17:31 +0000

    Adding bootstrap grid and vertical nav-links to home.php

--- views/home.php
+++ views/home.php
@@ -1,6 +1,24 @@
-  <div class="container">
-
-    <h1>Home page</h1>
+  <div class="container-fluid">
+    
+    <div class="row" id="homeRowContent">
+      <div class="col-sm-8" id="homeCol8" >
+        <h1>HOME PAGE</h1>
+      </div>
+      <div class="col-sm-4" id="homeCol4">
+        <nav class="nav flex-column">
+          <a class="nav-link" href="#">Add new client</a>
+          <a class="nav-link" href="#">Add new hairdresser</a>
+          <a class="nav-link" href="#">Book appointment</a>
+          <a class="nav-link" href="#">Cancel appointment</a>
+          <a class="nav-link" href="#">View clients</a>
+          <a class="nav-link" href="#">View hairdressers</a>
+          <a class="nav-link" href="#">View appointments</a>
+          <a class="nav-link" href="#">View schedule</a>
+          <a class="nav-link" href="#">Search</a>
+        </nav>
+      </div>
+    </div>
+    
 
   </div>
     
