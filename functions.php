30504e93ca44c1490883c3e7453ac730fcb69bea
Peter Cheung <perverskitty@mac.com>
2017-06-26 13:25:42 +0000

    Adding bootstrap table to display clients function.

--- functions.php
+++ functions.php
@@ -61,7 +61,62 @@
       
       if ($_SESSION['id'] > 0) {
         
-        echo 'Displaying clients content';
+        echo '<table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>Tel</th>
+              <th>Haidresser</th>
+              <th>Created</th>
+              <th>Changed</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Ally Mucha</td>
+              <td>female</td>
+              <td>ally@client.com</td>
+              <td>07540 555 666</td>
+              <td>Peter Cheung</td>
+              <td>2017-06-25 18:07:56</td>
+              <td>2017-06-25 18:07:56</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Fee Day</td>
+              <td>female</td>
+              <td>fee@client.com</td>
+              <td>07540 666 777</td>
+              <td>Ren Calder</td>
+              <td>2017-06-25 18:07:56</td>
+              <td>2017-06-25 18:07:56</td>
+            </tr>
+            <tr>
+              <<th scope="row">3</th>
+              <td>Sue Ochs</td>
+              <td>female</td>
+              <td>sue@client.com</td>
+              <td>07540 777 888</td>
+              <td>Lory Gill</td>
+              <td>2017-06-25 18:07:56</td>
+              <td>2017-06-25 18:07:56</td>
+            </tr>
+            <tr>
+              <<th scope="row">4</th>
+              <td>Ben Thomas</td>
+              <td>male</td>
+              <td>ben@client.com</td>
+              <td>07540 888 999</td>
+              <td>None</td>
+              <td>2017-06-25 18:07:56</td>
+              <td>2017-06-25 18:07:56</td>
+            </tr>
+          </tbody>
+        </table>';
         
       }
       
