e2a9c93a44dcd6d8f8a7a23c5a54c4da2830e549
Peter Cheung <perverskitty@mac.com>
2017-06-26 12:06:23 +0000

    Adding views for clients, hairdressers, services, appts, and schedules.

---
+++ views/clients.php
@@ -0,0 +1,17 @@
+  <div class="container-fluid">
+    
+    <div class="row" id="homeRowContent">
+      
+      <div class="col-sm-8" id="homeCol8" >
+        <h1>CLIENTS</h1>
+      </div>
+      
+      <div class="col-sm-4" id="homeCol4">
+        <?php displayNav(); ?>
+      </div>
+      
+    </div>
+    
+
+  </div>
+    
