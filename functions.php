2e68b71ee87b87652485b06d1855d0c08d1b74a0
Peter Cheung <perverskitty@mac.com>
2017-06-26 11:46:06 +0000

    Moving vertical nav-link html into a function

--- functions.php
+++ functions.php
@@ -22,13 +22,40 @@
     if ($_GET['function'] == "signout") {
     
       session_unset();
-    
     }
-  
-  
   }
 
-
+  // Display vertical nav-links function
+  function displayNav() {
+    
+    if (isset($_SESSION['id'])) {
+      
+      if ($_SESSION['id'] > 0) {
+        
+        echo '<nav class="nav flex-column">
+          <a class="nav-link" href="#">Add new client</a>
+          <a class="nav-link" href="#">Add new hairdresser</a>
+          <a class="nav-link" href="#">Book appointment</a>
+          <a class="nav-link" href="#">Cancel appointment</a>
+          <a class="nav-link" href="#">View clients</a>
+          <a class="nav-link" href="#">View hairdressers</a>
+          <a class="nav-link" href="#">View appointments</a>
+          <a class="nav-link" href="#">View schedule</a>
+          <a class="nav-link" href="#">Search</a>
+        </nav>';
+        
+      } 
+        
+    } else {
+        
+        echo '<nav class="nav flex-column">
+          <a class="nav-link" href="#">News</a>
+          <a class="nav-link" href="#">About us</a>
+        </nav>';
+      
+    }
+    
+  }
   
 
 ?>
