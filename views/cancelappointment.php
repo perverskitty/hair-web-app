983e098dcdfb1871735bd36e6c866123ba6fac35
Peter Cheung <perverskitty@mac.com>
2017-06-28 16:13:24 +0000

    Adding bootstrap form to  cancel appt view.

--- views/cancelappointment.php
+++ views/cancelappointment.php
@@ -4,7 +4,13 @@
       
       <div class="col-sm-8" id="homeCol8" >
         <h1>Cancel an appointment</h1>
-        
+        <form>
+          <div class="form-group">
+            <label for="cancel_id">Appointment ID</label>
+            <input type="number" class="form-control" id="cancel_id" placeholder="Enter ID" min="1">
+          </div>
+          <button type="button" class="btn btn-primary" id="cancel_appt_button">Cancel appointment</button>
+        </form>
       </div>
       
       <div class="col-sm-4" id="homeCol4">
